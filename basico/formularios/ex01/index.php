edffa12436b66f0298b3d4279302cdf7f9885e31
Matheus Tomaz <matheusdeveloperr@gmail.com>
2025-11-23 21:41:48 -0300

    Aula sobre validações de dados recebidos

--- basico/formularios/ex01/index.php
+++ basico/formularios/ex01/index.php
@@ -2,8 +2,36 @@
 // requires
 require 'functions.php';
 // MENSAGENS
-$erro = 'Erro no formulário';
-$sucesso = 'Sucesso no formulário';
+$erro = null;
+$sucesso = null;
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $texto = $_POST['texto'];
+
+    // Sanitização
+    $texto = htmlspecialchars($texto);
+
+    // Removendo espaços em branco
+    $texto = trim($texto);
+
+    // var_dump($texto);
+    if (filter_var($texto, FILTER_VALIDATE_EMAIL) == false) {
+        echo "O valor digitado não corresponde há um email válido";
+    } else {
+        echo "O valor digitado corresponde há um email";
+    }
+    
+    // Primeira validação - Se o campo está preenchido
+    if (empty($texto)) {
+        echo "O campo texto é obrigatório";
+    }elseif (strlen($texto) < 5 ) {
+        $erro = 'O campo precisa ter no mínimo 5 caracteres';
+    }elseif (strlen($texto) > 25) {
+        $erro = 'O campo deverá ter no máximo 25 caracteres';
+    }else {
+        $sucesso = 'Campo validado com sucesso';
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,18 +44,18 @@ $sucesso = 'Sucesso no formulário';
     <h1>Formulário</h1>
             <?php if (exibir_erro($erro)) : ?>
             <p style="color: red;">
-                <? echo $erro; ?>
+                <?php echo $erro; ?>
             </p>
         <?php endif; ?> 
         <?php if (exibir_erro($sucesso)) : ?>
             <p style="color: green;">
-                <? echo $sucesso; ?>
+                <?php echo $sucesso; ?>
             </p>
         <?php endif; ?>
     <form method="POST">
 
  
-        <input type="text" name="campo" id="campo">
+        <input type="text" name="texto" id="texto">
         <input type="submit" value="Enviar">
     </form>
 </body>
